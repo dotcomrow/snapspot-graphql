5a74048ee5a226c56f6a923799b76643b8e0c154
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-11-15 20:21:11 -0500

    removing unused
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,10 +1,6 @@
 import { mergeResolvers } from '@graphql-tools/merge';
 
 import * as Hello from '../modules/base/Query.resolvers.js'
-import * as User from '../modules/user/User.resolvers.js'
-import * as Config from '../modules/config/Config.resolvers.js'
-import * as Http from '../modules/http/Http.resolvers.js'
-import * as CMS from '../modules/cms/CMS.resolvers.js'
 import * as FetchPictures from '../modules/pictures/fetch-requests/fetch-pictures.resolvers.js'
 import * as ModifyPictures from '../modules/pictures/modify-requests/modify-pictures.resolvers.js'
 
